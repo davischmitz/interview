9c3b70e4c2f46cc0e983665f91c3c768facd589c
Davi Schmitz <davi.schmitz@sap.com>
2023-04-17 16:14:52 -0300

    Add js template

---
+++ autoscale.js
@@ -0,0 +1,9 @@
+// Function to find the number of
+// instances after completion
+function finalInstances(instances, arr) {}
+
+const instances = 2;
+const arr = [25, 23, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 76, 80];
+
+// Function Call
+finalInstances(instances, arr);
